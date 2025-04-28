78efffa453b272909ec6ec09fcd8ebe93d6f53b2
Samuel Lanzuela <744726@student.dusd.net>
2025-04-28 10:20:53 -0700

    ddd
    dddd

--- P2SoundAndEffectWilliams/Assets/Course Library/Scripts/PlayerController.cs
+++ P2SoundAndEffectWilliams/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerR6;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    
     // Start is called before the first frame update
     void Start()
     {
         playerR6 = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -16,7 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerR6.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerR6.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
