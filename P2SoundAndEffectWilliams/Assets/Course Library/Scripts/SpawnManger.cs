44f963bbd44025846fc366c79bca2311f815193b
Samuel Lanzuela <744726@student.dusd.net>
2025-05-08 10:00:41 -0700

    uyt
    tyuty

--- P2SoundAndEffectWilliams/Assets/Course Library/Scripts/SpawnManger.cs
+++ P2SoundAndEffectWilliams/Assets/Course Library/Scripts/SpawnManger.cs
@@ -6,16 +6,22 @@ public class SpawnManger : MonoBehaviour
 {
     public GameObject obstaclePerfab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-     
+    private float startDelay = 2;
+    private float repeatDelay = 2;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePerfab, spawnPos, obstaclePerfab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePerfab, spawnPos, obstaclePerfab.transform.rotation);
     }
 }
